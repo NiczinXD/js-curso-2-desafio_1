e55db23b72f9e370b5443337c24731a003bb58d0
NiczinXD <nicolasxpinheiro@gmail.com>
2024-05-04 09:21:23 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//SS - 29
+
+//Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+    let titulo = document.querySelector('h1'); 
+    titulo.innerHTML = 'Hora do desadio'   
+//Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+    function clicadoConsole() {
+        console.log('O botão foi clicado');
+    }
+//Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+    function clicadoAlert() {
+        alert('Eu amo JS');
+    }    
+//Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+    function clicadoPrompt() {
+        let cidade = prompt('Fale um nome de uma cidade do Brasil');
+        alert(`Estive em ${cidade} e lembrei de você`);
+    }    
+//Ao clicar no botão soma, peça 2 números e exiba o resultado da soma em um alerta.
+    function clicadoSoma() {
+        let numero1 = Number(prompt('me de um número'));
+        let numero2 = Number(prompt('me de outro número'));
+        let resultado = numero1 + numero2;
+        alert(`o número ${numero1} mais o ${numero2} é igual a ${resultado}.`);  
+    }
